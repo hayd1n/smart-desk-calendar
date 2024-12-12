eae6a9cc67dfd9cfcf0e30b799953896884c8e92
Hayden Chang <m831718@gmail.com>
2024-12-12 12:27:33 +0800

    chore(firmware): add sleep tool

--- firmware/src/sleep.rs
+++ firmware/src/sleep.rs
@@ -1,7 +1,6 @@
-use esp_idf_svc::nvs::{EspDefaultNvsPartition, EspNvs};
-
 pub mod common;
 
+#[allow(unreachable_code)]
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -10,5 +9,9 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-
+    log::info!("Entering deep sleep");
+    unsafe {
+        esp_idf_svc::sys::esp_deep_sleep_start();
+    }
+    log::error!("Deep sleep failed");
 }
